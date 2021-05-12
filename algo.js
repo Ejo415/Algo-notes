99ce919181075802e03fe08e7a136838c7c586ba
ejo415 <ejo7220@gmail.com>
2021-05-12 10:08:45 -0400

    problem description

---
+++ algo.js
@@ -0,0 +1,3 @@
+// Anagrams
+
+// Given two strings, write a function to determine if the second string is an anagram of the first,An anagram is a word, phrase or name formed by rearranging the letters of another. Such as cinema formed from iceman.
