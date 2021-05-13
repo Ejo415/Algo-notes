810f7e06c50bb6d18c9f13ec352ad738a10d3545
ejo415 <ejo7220@gmail.com>
2021-05-13 11:01:58 -0400

    added refactor

--- algo.js
+++ algo.js
@@ -57,18 +57,34 @@
 
 // Implement a function called countUniqueValues which accepts a sorted Array, and counts the unique values in the Array. There can be negative numbers in the Array, but it will always be sorted.
 
-function countUniqueValues(arr) {
-  if (arr.length === 0) return 0;
-  let left = 0;
-  let right = 1;
-  while (right < arr.length) {
-    if (arr[left] === arr[right]) {
-      right++;
-    } else {
-      left++;
-      arr[left] = arr[right];
-      right++;
-    }
-  }
-  return left + 1;
-}
+//my solution
+
+// function countUniqueValues(arr) {
+//   if (arr.length === 0) return 0;
+//   let left = 0;
+//   let right = 1;
+//   while (right < arr.length) {
+//     if (arr[left] === arr[right]) {
+//       right++;
+//     } else {
+//       left++;
+//       arr[left] = arr[right];
+//       right++;
+//     }
+//   }
+//   return left + 1;
+// }
+
+//refactor
+
+// function countUniqueValues(arr) {
+//     if (arr.length === 0) return 0
+//     let i = 0;
+//     for (let j = 1; j < arr.length; j++) {
+//         if (arr[i] !== arr[j]) {
+//             i++
+//             arr[i] = arr[j]
+//         }
+//     }
+//     return i + 1;
+// }
