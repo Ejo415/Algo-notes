cdc53eb3072144a076829c560f887d473897a00d
ejo415 <ejo7220@gmail.com>
2021-05-13 10:26:48 -0400

    take 1 solution

--- algo.js
+++ algo.js
@@ -56,3 +56,20 @@
 // Multiple Pointers
 
 // Implement a function called countUniqueValues which accepts a sorted Array, and counts the unique values in the Array. There can be negative numbers in the Array, but it will always be sorted.
+
+function countUniqueValues(arr) {
+    if (arr.length === 0) return 0;
+    let left = 0;
+    let right = 1;
+    while (right < arr.length) {
+        if (arr[left] === arr[right])
+        right++
+    } else {
+        left++
+        arr[left] = arr[right]
+        right++
+    }
+    console.log(left+1)
+    return left+1
+  }
+  
