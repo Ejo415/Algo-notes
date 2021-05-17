96f9a535a45eb5a62e59168ac7266e1562c4c0dc
ejo415 <ejo7220@gmail.com>
2021-05-17 13:26:44 -0400

    refactored variables in second anagram loop

--- algo.js
+++ algo.js
@@ -2,27 +2,26 @@
 
 // Given two strings, write a function to determine if the second string is an anagram of the first,An anagram is a word, phrase or name formed by rearranging the letters of another. Such as cinema formed from iceman.
 
-// function validAnagram(str1, str2) {
-//   if (str1.length !== str2.length) {
-//     return false;
-//   }
-//   let word1 = [];
-//   let word2 = [];
-//   for (let letter of str1) {
-//     word1[letter] = (word1[letter] || 0) + 1;
-//   }
-//   for (let val of str2) {
-//     word2[val] = (word2[val] || 0) + 1;
-//   }
-//   console.log(word1);
-//   console.log(word2);
-//   for (let key in word1) {
-//     if (word1[key] !== word2[key]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
+function validAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  let word1 = {};
+  let word2 = {};
+  for (let letter of str1) {
+    word1[letter] = (word1[letter] || 0) + 1;
+  }
+  for (let letter of str2) {
+    word2[letter] = (word2[letter] || 0) + 1;
+  }
+
+  for (let key in word1) {
+    if (word1[key] !== word2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 //refactored solution
 
