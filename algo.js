f0d4d17dce8da2e97fb7cac4b20eefdf4aa9fe5d
ejo415 <ejo7220@gmail.com>
2021-05-28 16:09:08 -0400

    added psudeo code and solution

--- algo.js
+++ algo.js
@@ -125,3 +125,21 @@ function areThereDuplicates() {
 }
 
 // Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be  more than one pair that matches the average target.
+
+//defne pointers
+//add and average both pointers
+//if average === n return true
+//if false move pointer
+
+function averagePair(arr, n) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let avg = (arr[left] + arr[right]) / 2;
+    if (avg === n) return true;
+    else if (avg > n) right--;
+    else left++;
+  }
+  return false;
+}
