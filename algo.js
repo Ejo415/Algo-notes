f6a523ab50a33df5c78ba0a0979da340c4d9218f
ejo415 <ejo7220@gmail.com>
2021-06-01 12:41:56 -0400

    new problem + beginning of solution

--- algo.js
+++ algo.js
@@ -162,3 +162,17 @@ function isSubsequence(str1, str2) {
   }
   return false;
 }
+
+// Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+
+// Note that a subarray must consist of CONSECUTIVE elements from the original array. In the first example below [100, 200, 300] is a subarray of the orignal array, but [100, 300] is not
+
+//         maxSubarraySum([100,200,300,400], 2) -> 700
+//         maxSubarraySum([1,4,2,10,23,3,1,0,20], 4) -> 39
+//         maxSubarraySum([2,3], 3 ) -> null
+
+function maxSubarraySum(arr, num) {
+  if (num > arr.length) {
+    return null;
+  }
+}
