07fdc80ad148bcfbf860b061214f4c16ba084c86
ejo415 <ejo7220@gmail.com>
2021-06-01 14:36:02 -0400

    solution

--- algo.js
+++ algo.js
@@ -172,7 +172,16 @@ function isSubsequence(str1, str2) {
 //         maxSubarraySum([2,3], 3 ) -> null
 
 function maxSubarraySum(arr, num) {
-  if (num > arr.length) {
-    return null;
+  let maxSum = 0;
+  let tempSum = 0;
+  if (num > arr.length) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
   }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
 }
