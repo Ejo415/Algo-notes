437f8ca66a5250a0ca251a3213f41872cf704a38
ejo415 <ejo7220@gmail.com>
2021-06-01 16:17:02 -0400

    solution

--- algo.js
+++ algo.js
@@ -221,3 +221,20 @@ function minSubArrayLen(arr, num) {
 // findLongestSubstring('rithmschool') -> 7
 // findLongestSubstring('bbbbbb') -> 1
 // findLongestSubstring('findlongestsubstring') -> 8
+
+function findLongestSubstring(str) {
+  if (str.length === 0) return 0;
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    longest = Math.max(longest, i - start + 1);
+    seen[char] = i + 1;
+  }
+  return longest;
+}
