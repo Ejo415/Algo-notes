f33d9fb99f0d29d9121b6da76d6f12cb5136d9e5
ejo415 <ejo7220@gmail.com>
2021-06-16 10:02:46 -0400

    empty function

--- algo.js
+++ algo.js
@@ -250,3 +250,5 @@ function reverse(x) {
 }
 
 // Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+function subtractProductAndSum(n) {}
