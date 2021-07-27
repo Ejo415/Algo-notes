f2f2aa1facc52337fe8c7716c5651ca2ba6956b6
ejo415 <ejo7220@gmail.com>
2021-07-27 14:29:22 -0400

    new function

--- algo.js
+++ algo.js
@@ -336,3 +336,5 @@ const twoSum = function (nums, target) {
 // You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
 // Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+var maxProfit = function (prices) {};
