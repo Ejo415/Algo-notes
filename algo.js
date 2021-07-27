b7926272ed3458aa6c8cd42510a42f7e42ebf4f1
ejo415 <ejo7220@gmail.com>
2021-07-27 14:30:58 -0400

    else if logic and return

--- algo.js
+++ algo.js
@@ -341,7 +341,13 @@ var maxProfit = function (prices) {
   let minBuyPrice = Infinity;
   let maxProfit = 0;
 
-  for (let price of prices){
-    if (price < minBuyPrice){
-        minBuyPrice = price
+  for (let price of prices) {
+    if (price < minBuyPrice) {
+      minBuyPrice = price;
+    } else if (price - minBuyPrice > maxProfit) {
+      maxProfit = price - minBuyPrice;
+    }
+  }
+
+  return maxProfit;
 };
