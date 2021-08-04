d4e00d66da08c4402e9f32e50434ff998d73062f
ejo415 <ejo7220@gmail.com>
2021-08-04 16:30:25 -0400

    new problem

--- algo.js
+++ algo.js
@@ -351,3 +351,7 @@ var maxProfit = function (prices) {
 
   return maxProfit;
 };
+
+// Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+
