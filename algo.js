f3d0b7db8ff543640c6dd2f96998683744f8b2c8
ejo415 <ejo7220@gmail.com>
2021-08-04 16:31:53 -0400

    empty function

--- algo.js
+++ algo.js
@@ -354,4 +354,4 @@ var maxProfit = function (prices) {
 
 // Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
-
+var firstUniqChar = function (s) {};
