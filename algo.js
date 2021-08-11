91857a438f1ed33ea81bdbf2bb3bd38b12c089f8
ejo415 <ejo7220@gmail.com>
2021-08-11 16:09:24 -0400

    second loop

--- algo.js
+++ algo.js
@@ -379,8 +379,14 @@ var firstUniqChar = function (s) {
 
 var intersection = function (nums1, nums2) {
   let firstSet = new Set();
+
   for (let num of nums1) {
     firstSet.add(num);
   }
   let commonSet = new Set();
+  for (let num of nums2) {
+    if (firstSet.has(num)) {
+      commonSet.add(num);
+    }
+  }
 };
