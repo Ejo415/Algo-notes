a651119a9012e20053c796c0a9e706b2523c24a5
ejo415 <ejo7220@gmail.com>
2021-08-11 16:06:34 -0400

    new problem

--- algo.js
+++ algo.js
@@ -374,3 +374,5 @@ var firstUniqChar = function (s) {
   }
   return result;
 };
+
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
